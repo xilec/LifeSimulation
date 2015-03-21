2e7e42e2a1a1e17dc55897f44cfcb963dcfd020d
Evgeny Khudoba <evgeny.khudoba@rubius.com>
2015-03-21 16:31:45 +0600

    First try rewriting algorithm simulation of life on Nemerle (not finished)

---
+++ Nemerle/LIfeSimulation/SyntaxTest/SytaxTestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxTest
+{
+    public class SytaxTestClass
+    {
+        public static void Test(int a)
+        {
+            var random = new Random();
+        }
+    }
+}
